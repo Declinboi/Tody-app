e2a9b60b20be5c1e02143456490903c18f9ff8a4
declinboi <jacobclinton2234@gmail.com>
2025-08-20 11:52:25 +0100

    app ui

---
+++ tody/packages/app_ui/lib/app_ui.dart
@@ -0,0 +1,15 @@
+/// A Very Good Project created by Very Good CLI.
+// ignore_for_file: eol_at_end_of_file
+
+library;
+
+export 'src/colors/app_colors.dart';
+export 'src/constants/constants.dart';
+export 'src/extensions/extensions.dart';
+export 'src/generated/generated.dart';
+export 'src/spacing/app_spacing.dart';
+export 'src/theme/theme.dart';
+export 'src/typography/typography.dart';
+export 'src/widgets/widgets.dart';
+
+
